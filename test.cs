bf80ceeea78204f80343edcdda0ad1f3d454a889
palfrey <palfrey@tevp.net>
2007-04-10 19:27:25 +0100

    test now does an xmlparse
    darcs-hash:20070410182725-90518-8babcac6f20fd49177b1b0103eb27c378593375d.gz

--- test.cs
+++ test.cs
@@ -2,42 +2,73 @@ using System;
 using System.IO;
 using NonValidating;
 using System.Xml;
+using System.Xml.Xsl;
+using System.Collections.Generic;
 
 public class test
 {
+	public class StringHash: Dictionary <string,string> /*Hashtable*/
+	{
+	}
+
+	public static StringHash[] xmlParser(string input, string separator, StringHash mappings)
+	{
+		List<StringHash> rows = new List<StringHash>();
+		//XmlTextReader reader = new XmlTextReader(new SafeStringReader(input));
+		NonValidatingReader reader = new NonValidatingReader(input);
+		string top = reader.NameTable.Add(separator);
+		while (reader.Read()) 
+		{
+			if (reader.NodeType == XmlNodeType.Element && reader.Name.CompareTo(top)==0) 
+			{
+				StringHash ret = new StringHash();
+				reader.Read();
+				string element = null;
+				while (reader.Name.CompareTo(top)!=0)
+				{
+					switch(reader.NodeType)
+					{
+						case XmlNodeType.Element:
+							element = reader.Name;
+							if (element!=null && mappings.ContainsKey(element))
+							{
+								if (mappings[element] == null) /* therefore, skip */
+									reader.ReadOuterXml();	
+								else
+									element = mappings[element];
+							}
+							break;
+						case XmlNodeType.Text:
+							if (!ret.ContainsKey(element))
+								ret.Add(element,reader.Value);
+							break;
+					}
+					if (reader.Read()==false)
+						break;
+				}
+				rows.Add(ret);
+				continue;
+			}
+		}
+		return rows.ToArray();
+	}
+
+	private static void parseSearchResults(string curr)
+	{
+		StringHash mappings = null;
+		if (mappings == null)
+		{
+			mappings = new StringHash();
+			mappings.Add("bz:id","ID");
+		}
+		StringHash[] core = xmlParser(curr,"bz:bug",mappings);
+	}
+
 	public static void Main(string[] args)
 	{
 		StreamReader inFile = new StreamReader(args[0]);
-		string ret = inFile.ReadToEnd();
+		string xsl = inFile.ReadToEnd();
 		inFile.Close();
-		NonValidatingReader reader = new NonValidatingReader(ret);
-		string top = "bz:bug";
-		while (reader.Read()) 
-		{
-			if (reader.NodeType == XmlNodeType.Element) 
-			{
-				Console.WriteLine("some element: {0}",reader.Name);
-				if (reader.Name.Equals(top)) 
-				{
-					reader.Read();
-					Console.WriteLine("Equals");
-					string element = null;
-					while (!reader.Name.Equals(top))
-					{
-						switch(reader.NodeType)
-						{
-							case XmlNodeType.Element:
-								element = reader.Name;
-								Console.WriteLine("element: {0}",element);
-								break;
-							case XmlNodeType.Text:
-								Console.WriteLine("text: {0}",reader.Value);
-								break;
-						}
-						reader.Read();
-					}
-				}      
-			}
-		}
+		parseSearchResults(xsl);
 	}
 }
